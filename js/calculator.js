99f12a412dd4bdcd59a4a6cd2773df7da0551e32
teklithuania <61139751+teklithuania@users.noreply.github.com>
2020-03-09 18:34:04 +0200

    Initial commit

--- js/calculator.js
+++ js/calculator.js
@@ -43,3 +43,12 @@ function bonus(num10) {
     let result = percentOf(squered, half);
 }
 bonus(5);
+
+function printTime() {
+    var d = new Date();
+    var hours = d.getHours();
+    var mins = d.getMinutes();
+    var secs = d.getSeconds();
+    document.body.innerHTML = hours+":"+mins+":"+secs;
+}
+setInterval(printTime, 1000);
